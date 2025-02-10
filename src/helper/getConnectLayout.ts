2a5979541f72d529d1961ab8f9305277bc9b03ea
Devansh <dkansagra04@gmail.com>
2025-02-10 19:16:45 +0530

    Fix issue 81

--- src/helper/getConnectLayout.ts
+++ src/helper/getConnectLayout.ts
@@ -3,6 +3,7 @@ import { PreviewBlockWithPreview, PreviewBlock } from "@rocket.chat/ui-kit";
 import { ElementBuilder } from "../lib/ElementBuilder";
 import { BlockBuilder } from "../lib/BlockBuilder";
 import { Notion } from "../../enum/Notion";
+import { OnInstallContent } from "../../enum/messages";
 
 export function getConnectPreview(
     appId: string,
@@ -17,14 +18,16 @@ export function getConnectPreview(
     const workspace_icon_url = workspace_icon?.startsWith("/")
         ? `${Notion.WEBSITE_URL}${workspace_icon}`
         : workspace_icon?.startsWith("http")
-        ? `${workspace_icon}`
-        : undefined;
-    const thumb = workspace_icon_url ? { url: workspace_icon_url } : undefined;
-    const title = [
-        `** [**${workspace_name}**](${Notion.WEBSITE_URL})**`,
-        "**Connected to Workspace**",
+          ? `${workspace_icon}`
+          : undefined;
+    const thumb = workspace_icon_url
+        ? { url: workspace_icon_url }
+        : { url: OnInstallContent.PREVIEW_IMAGE.toString() };
+    const title = [""];
+    const description = [
+        `** [${workspace_name}](${Notion.WEBSITE_URL})**`,
+        "**Connected to Workspace** \n",
     ];
-    const description = [""];
     const avatarElement = elementBuilder.addImage({
         imageUrl: avatar_url as string,
         altText: name as string,
